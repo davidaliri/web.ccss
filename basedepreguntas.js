99961ab4a7881c40a2f52402bc953e2fea7e337f
davidaliri <davidaliri95@gmail.com>
2024-12-13 14:35:31 -0500

    Add files via upload

---
+++ basedepreguntas.js
@@ -0,0 +1,150 @@
+basedepreguntas = [
+    {
+
+      pregunta: `Analiza las fuentes; luego responde las preguntas 1 y 2.`,
+      imagen: `https://i.ibb.co/cK9WH0c/Cam-Scanner-13-12-2024-11-48.jpg`,
+      respuesta: `		De la clase noble,		`,
+      distractores: [`		Esclavo libre.		`, `		Caballerango  		`, `	Siervo.	`],
+      
+      
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `2.	Durante el feudalismo, las tierras de cultivo fueron de propiedad exclusiva de: `,
+      respuesta: `	El señor feudal.			`,
+      distractores: [`	El Vasallo.		`, `	Del campesino.		 	`, `	Del militar.			`],
+      ayuda: ``,
+      ayudaImg: ``,
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/8MDFvc5/Cam-Scanner-13-12-2024-11-59.jpg` ,
+      pregunta: `3.	El feudalismo tuvo como actividades económicas:`,
+      respuesta: `	Agricultura y la ganadería.		`,
+      distractores: [`	Pesca y el comercio. 		`, `	Agrícola y comercial.		`, `		Ganadera y comercial.		`],	
+        
+        
+       
+    }
+    ,
+    {
+      imagen: `  ` ,
+      pregunta: ` 4.	¿Qué relación encuentras entre ambos factores para el desarrollo de los viajes expedicionarios?	`,
+      respuesta: `		La búsqueda de nuevas rutas comerciales fue facilitada por el avance tecnológico.	 	`,
+      distractores: [`	Con la clausura de la ruta de la seda se perdió el acceso al oriente.		`, `	Los avances tecnológicos en navegación, motivaron las expediciones			`, `	Los portugueses y españoles aprovecharon los avances tecnológicos para realizar expediciones.				`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/xGbdwPm/Cam-Scanner-13-12-2024-12-09.jpg ` ,
+      pregunta: `5.	¿En qué edad de la historia se dan los mayores procesos de la expansión europea y quienes la realizaron?  `,
+      respuesta: `		Edad moderna por españoles y portugueses,.	`,
+      distractores: [`		Edad moderna por ingleses y franceses.		`, `		Edad media por ingleses y franceses		`, `	Edad moderna por españoles e ingleses.		 	`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/f9JVrg5/Cam-Scanner-13-12-2024-12-16.jpg` ,
+      pregunta: `  `,
+      respuesta: `Que a finales del siglo XV aún no se conocía la existencia de América.	 		`,
+      distractores: [`	Que en el siglo XVI se desconocía la existencia del continente americano.		`, `		Que en siglo XV se descubrió el continente asiático incluido la India.		`, `		A finales del siglo XVI se iniciaron los proyectos expedicionarios en Europa.		`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/vD5rKBt/Cam-Scanner-13-12-2024-12-25.jpg` ,
+      pregunta: ` `,
+      respuesta: `		El énfasis en el libre ejercicio de la razón y la autonomía humana. 	 `,
+      distractores: [`	La reforma interna de la Iglesia Católica considerando aspectos relevantes de la Reforma protestante.				`, `	La práctica de normas y acuerdos en favor de la sociedad masculina		 	`, `	El desarrollo de las primeras máquinas y del motor de combustión interna.		 	`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/3pk5VyM/Cam-Scanner-13-12-2024-12-28.jpg` ,
+      pregunta: `8.	¿Qué importancia económica tuvo para España y Europa esta nueva tuta de navegación? `,
+      respuesta: `		Impulsó el desarrollo y apogeo económico monopolizante de España, 		`,
+      distractores: [`		Estableció tas relaciones equitativas económicas entre América y España, 		`, `	Permitió el conocimiento de los productos dc América,		`, `	Incorporó a América al desarrollo cultural y social de Europa,  `],
+    }
+    ,
+    {
+      imagen: ` https://i.ibb.co/59r2P8z/Cam-Scanner-13-12-2024-12-33.jpg` ,
+      pregunta: ` `,
+      respuesta: `	La división de América entre dos estados monopolizadores.	   	 `,
+      distractores: [`		América se convirtió en un territorio autónomo.			`, `		América fue invadido por los portugueses.	 		`, `	Los nuevos territorios descubiertos pertenecían al clero.	 	`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/hB2pjjc/Cam-Scanner-13-12-2024-12-41.jpg` ,
+      pregunta: ` `,
+      respuesta: `		La crisis demográfica y social en el mundo medieval.		`,
+      distractores: [`		Crecimiento demográfico en la etapa el feudalismo,		`, `		Desarrollo tecnológico de la agricultura asistida,			`, `		La estabilidad económica en el clero y nobleza medioeval.		`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/ngY7Ynd/foto.jpg` ,
+      pregunta: `11.	¿cuál es el impacto negativo que podría generar las actividades humanas? `,
+      respuesta: `	La expansión de la zona urbana cerca al rio.			`,
+      distractores: [`	Incremento del dióxido de carbono en el campo.			`, `	Aumento de la basura y residuos sólidos cerca al rio.		`, `	Aumento del recurso hídrico al no haber más cultivos que irrigar.			`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: ` 12.	Según este informe, ¿Qué está sucediendo a partir del 2017?`,
+      respuesta: `	b)	Que durante el año 2017 la intensidad de la tala ha sido mayor debido a la  minería ilegal y la agricultura		 	`,
+      distractores: [`	a)	Que durante el año 2017 el nivel de avance de la tala de bosques en Madre de Dios no es considerable. 		`, `	c)	Que durante el año 2017 la Amazonía está en grave riesgo de ser desaparecida por la minería ilegal 	`, `	d)	Que durante el año 2017 los mineros ilegales no respetan al medioambiente en la selva amazónica.		`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/k1qNpVn/Cam-Scanner-13-12-2024-12-59.jpg	`,
+      pregunta: `13.	¿cuál es el impacto negativo que podría generar las actividades humanas?	 `,
+      respuesta: `	Aumento de la basura y residuos sólidos cerca al rio.	` ,
+      distractores: [`		Incremento del dióxido de carbono en el campo.			`, `		La expansión de la zona urbana cerca al rio.		`, `		Aumento del recurso hídrico al no haber más cultivos que irrigar.		`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/THSX96J/Cam-Scanner-13-12-2024-13-53.jpg` ,
+      pregunta: `	14.	Marca la afirmación correcta.`,
+      respuesta: `La ciudad de Buenos Aires y Sídney se encuentra en el hemisferio sur.		`,
+      distractores: [`	La ciudad de Lima y Nueva York se encuentran en el hemisferio norte	`, `		La Ciudad del Cabo se encuentra en el meridiano de Greenwich	 		`, `		La ciudad de Moscú y Lima se encuentran en el hemisferio norte	`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/vHtn5DX/Cam-Scanner-13-12-2024-13-56.jpg` ,
+      pregunta: `	`,
+      respuesta: `		Puna — Chala —Suni - Omagua. 		`,
+      distractores: [`Puna — Chala — Quechua — Rupa Rupa.			`, `	Puna —Yunga - Quechua - Rupa rupa.				`, `	Puna — Quechua — Rupa rupa —Omagua.		`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/kKtn2zC/Cam-Scanner-13-12-2024-14-00.jpg` ,
+      pregunta: `16.	Marca la afirmación incorrecta. `,
+      respuesta: `		El mapa muestra que la región de Lima será la más beneficiada por ser la capital del país.	 		`,
+      distractores: [`		El mapa muestra las regiones que pueden ser afectadas por el fenómeno del niño.	`, `		El mapa muestra que la región de Ancash es la que tiene la mayor cantidad de intervenciones.		`, `El mapa muestra la cantidad de intervenciones que realizará el gobierno ante la llegada del fenómeno del niño. 		`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/6YTdJ1Y/Cam-Scanner-13-12-2024-14-04.jpg` ,
+      pregunta: `17.	Según el caso presentado ¿Qué acciones ie hubieran permitido a Juan a evitar la pérdida en su casa? `,
+      respuesta: `	Construir su vivienda tomando en cuenta el mapa de zonificacion urbana,		 	`,
+      distractores: [`		Construir su vivienða de un material mas resistente, 	`, `		Construir su vivienda en las laderas de los cerros mas altos para evitar la inundacion.		`, `Construir su vivienda de un material barato para no sentir significativamente el daño.		`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/nPzWbK6/Cam-Scanner-13-12-2024-14-11.jpg` ,
+      pregunta: `	`,
+      respuesta: `		La familia es un agente económico deficitario		`,
+      distractores: [`	Los ingresos y egresos de la familia son similares	 		`, `		La familia puede invertir para mejorar su economía.			`, `		La familia es un agente económico superavitario.		`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/mSwNJGk/Cam-Scanner-13-12-2024-14-16.jpg` ,
+      pregunta: `19.	Esta afirmación hace referencia a:  `,
+      respuesta: `		A comer todos los fines de semana pollo a la brasa.			`,
+      distractores: [`	El pago de los servicios básicos.			`, ` 	El pago de un crédito para pagar la casa	 	`, `	La compra de alimentos de primera necesidad.		`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/hLZtFnw/Cam-Scanner-13-12-2024-14-20.jpg` ,
+      pregunta: ` `,
+      respuesta: ` 		José se siente preocupado porque podría sobre endeudarse debido a que podría gastar más de lo que gana.		`,
+      distractores: [`		José está contento de tener tres tarjetas de crédito porque le ayudaría a comprar muchas cosas que necesita.	`, `	está preocupado de tener tres tarjetas de crédito porque se podría olvidar las contraseñas de sus tarjetas.		`, `		José se siente preocupado porque no sabe que comprar debido a que tiene muchas tarjetas.  			`],
+    }
+ 
+  ];
